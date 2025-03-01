8c716d080de78d8d0458bb5af508ca8c6838c400
Maximilian Theiner <theiner.maximilian@gmail.com>
2025-03-01 11:46:58 +0100

    added PyInstaller build

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Annuity based loan calculator
 - Install Poetry by using the command line: ``(Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | python -``
 - Install the dependecies by running ``poetry install`` in the working directory
 - Run ``poetry run python serve.py`` to start Flask
-- Execute ``http://127.0.0.1:5000/calc?principal=200000&duration=200&nom_intr=2.0&rdmp_intr=3.0&repay_amt=5000&repay_period=0`` in your Browser to retrieve an example repayment plan
+- Execute ``http://127.0.0.1:5000/calc?principal=200000&duration=200&nom_intr=2.0&rdmp_intr=3.0&repay_amt=5000&repay_period=0`` in your browser to retrieve an example repayment plan
+- Run ``pyinstaller --onefile serve.py`` to create an executable. Makes sure to add an exception to your firewall.
--- serve.py
+++ serve.py
@@ -46,12 +46,12 @@ def predict_user_questions():
         rdmp_intr = params.get("rdmp_intr", type=float)
         repay_amt = params.get("repay_amt", default=0, type=int)
         repay_period = params.get("repay_period", default=0, type=int)
-        filename = secure_filename(params.get("filename", default="tilgungsplan.xlsx"))
+        filename = params.get("filename", default="tilgungsplan.xlsx")
 
         repayment_plan = calc_repayment_plan(principal, nom_intr, rdmp_intr, repay_amt, repay_period, duration)
         repayment_plan.to_excel(filename)
 
-        filename = secure_filename(filename)
+        filename = os.path.join(os.getcwd(), filename)
         if os.path.isfile(filename):
             return send_file(filename, as_attachment=True)
         else:
@@ -71,4 +71,5 @@ if __name__ == '__main__':
     scheduler.start()
 
     num_workers = cpu_count()
+    print("Starting server..")
     serve(app, host='0.0.0.0', port=configuration["port"], threads=num_workers, connection_limit=1000000)
